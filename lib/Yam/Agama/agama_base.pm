91cdc2c16f35da0ba127f0b6e40953a6206d062f
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2025-05-14 09:42:09 +0200

    Removed select console on upload_agama_logs

--- lib/Yam/Agama/agama_base.pm
+++ lib/Yam/Agama/agama_base.pm
@@ -17,8 +17,6 @@ sub post_fail_hook {
 }
 
 sub upload_agama_logs {
-    select_console 'install-shell';
-
     if (script_run("test -d /run/agama/scripts") == 0) {
         script_run("tar czvf /tmp/agama_scripts.tar.gz /run/agama/scripts/*", {timeout => 60});
         upload_logs("/tmp/agama_scripts.tar.gz", failok => 1);

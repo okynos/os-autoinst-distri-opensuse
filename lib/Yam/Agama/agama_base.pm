aa3f6c1098c47efde049b47010c6b733e0d5a122
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2025-08-25 15:38:10 +0200

    Added timeout to upload logs

--- lib/Yam/Agama/agama_base.pm
+++ lib/Yam/Agama/agama_base.pm
@@ -23,6 +23,7 @@ sub upload_agama_logs {
         script_run("tar czvf /tmp/agama_scripts.tar.gz /run/agama/scripts/*", {timeout => 60});
         upload_logs("/tmp/agama_scripts.tar.gz", failok => 1);
     }
+    save_and_upload_log('agama config show', {timeout => 2400});
     save_and_upload_log('agama config show > /tmp/agama-config.json', "/tmp/agama-config.json", {timeout => 60});
     script_run("agama logs store -d /tmp/agama-logs", {timeout => 60});
     upload_logs("/tmp/agama-logs.tar.gz", failok => 1);

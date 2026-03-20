f62d9ff9da8c759c1f795e55b43528e90688d0f9
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2026-03-20 11:18:36 +0100

    Added split by = instead of whitespaces

--- lib/autoyast.pm
+++ lib/autoyast.pm
@@ -808,7 +808,9 @@ sub generate_json_profile {
     my $profile_path = get_required_var('CASEDIR') . "/data/" . $profile;
 
     my @profile_options = map { "--tla-" . (/true|false/ ? "code" : "str") . " $_ " }
-      split(' ', trim(get_var('AGAMA_PROFILE_OPTIONS')));
+      map { join('=', split(/=/, $_, 2));
+      } trim(get_var('AGAMA_PROFILE_OPTIONS'));
+      #split(' ', split(/=/, trim(get_var('AGAMA_PROFILE_OPTIONS'))), 2);
     diag "jsonnet @profile_options $profile_path";
     record_info("JSONNET Command", "jsonnet @profile_options $profile_path");
     my $profile_content = `jsonnet @profile_options $profile_path`;
